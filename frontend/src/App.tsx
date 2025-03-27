69f7bef2f7b8fc769c13888c51d7843a495da3e3
Surya-majhi32038 <suryamajhi32038@gmail.com>
2025-03-28 01:28:34 +0530

    Add Test component and update Footer for responsive design; include lucide-react icons

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Landing from './screen/Landing'
 import Game from './screen/Game'
+// import Test from './components/Test'
 
 function App() {
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,8 +7,9 @@ export default {
   theme: {
     extend: {
         screens: {
-            md: '900px', // Change `md` to start at 850px instead of 768px
-          },
+            md: '401px', // Change `md` to start at 850px instead of 768px
+            ph: { max: '400px' }, // Custom media query for phone screens
+        },
     },
   plugins: [],
 }
