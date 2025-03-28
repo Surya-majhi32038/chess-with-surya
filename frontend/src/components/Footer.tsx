fd24af777c4b17f48a5018685f97c440348b6cb1
Surya-majhi32038 <suryamajhi32038@gmail.com>
2025-03-28 17:49:44 +0530

    Update Footer component styles for improved layout and responsiveness also complete the landing page layout for phone

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -11,9 +11,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const Footer = () => {
   return (
-    <footer className="mt-10 mb-5">
+    <footer className="mt-10 mb-3">
       <div className="text-[#8D8C8A] text-xs  flex flex-col items-center">
-      <div className="flex items-center flex-wrap justify-center text-nowrap gap-2 h-auto font-medium text-gray-400 text-sm">
+      <div className="flex items-center flex-wrap justify-center text-nowrap  h-auto font-medium text-[#989795] text-xs gap-1">
           <p className="hover:text-white cursor-pointer">Support</p>
           <span className="text-lg leading-none">•</span>
           <p className="hover:text-white cursor-pointer">Chess Terms</p>
@@ -41,13 +41,11 @@ export const Footer = () => {
           <p className="hover:text-white cursor-pointer">Surya Majhi ( copy of Chess.com ) © 2025</p>
         </div>
 
-
-
         {/* different social media platform */}
 
-        <div className="flex mt-5 items-center  space-x-2 text-[#989795]">
+        <div className="flex mt-5 items-center ph:space-x-3  space-x-4 text-[#989795]">
           <FontAwesomeIcon
-            className="text-2xl ph:w-[28px] ph:h-[28px] hover:text-white cursor-pointer transition duration-300"
+            className="w-[28px] h-[28px] hover:text-white cursor-pointer transition duration-300"
             icon={faApple}
             onClick={() =>
               window.open("https://www.chess.com/play/apps/ios", "_self")
@@ -55,7 +53,7 @@ export const Footer = () => {
           />
 
           <FontAwesomeIcon
-            className="text-2xl ph:w-[28px] ph:h-[28px] hover:text-green-500 cursor-pointer transition duration-300"
+            className="w-[28px] h-[28px] hover:text-green-500 cursor-pointer transition duration-300"
             icon={faAndroid}
             onClick={() =>
               window.open("https://www.chess.com/play/apps/android", "_self")
@@ -65,7 +63,7 @@ export const Footer = () => {
           {/* Divider Line */}
           <div className="h-6 w-px bg-gray-500"></div>
           <FontAwesomeIcon
-            className="text-3xl ph:w-[28px] ph:h-[28px] hover:text-white cursor-pointer transition duration-300"
+            className="w-[28px] h-[28px] hover:text-white cursor-pointer transition duration-300"
             icon={faTiktok}
             onClick={() =>
               window.open("https://www.tiktok.com/notfound", "_self")
@@ -73,7 +71,7 @@ export const Footer = () => {
           />
 
           <FontAwesomeIcon
-            className="text-3xl ph:w-[28px] ph:h-[28px] hover:text-white cursor-pointer transition duration-300"
+            className="w-[28px] h-[28px] hover:text-white cursor-pointer transition duration-300"
             icon={faXTwitter}
             onClick={() =>
               window.open("https://x.com/chesscom", "_self")
@@ -81,27 +79,27 @@ export const Footer = () => {
           />
           <FontAwesomeIcon
             icon={faYoutube}
-            className="text-3xl ph:w-[28px] ph:h-[28px] hover:text-red-600 cursor-pointer transition duration-300"
+            className="w-[28px] h-[28px] hover:text-red-600 cursor-pointer transition duration-300"
             onClick={() =>
               window.open("https://www.youtube.com/user/wwwChesscom", "_self")
             }
           />
 
           <i
-            className="fa fa-twitch ph:w-[28px] ph:h-[28px] text-2xl hover:text-purple-500 cursor-pointer"
+            className="fa fa-twitch text-2xl hover:text-purple-500 cursor-pointer"
             onClick={() =>
               window.open("https://www.twitch.tv/chess", "_self")
             }
           ></i>
           <i
-            className="fa fa-instagram ph:w-[28px] ph:h-[28px] text-2xl hover:text-pink-600 cursor-pointer"
+            className="fa fa-instagram text-2xl hover:text-pink-600 cursor-pointer"
             onClick={() =>
               window.open("https://www.instagram.com/wwwchesscom/#", "_self")
             }
           ></i>
           {/* <i className="fa fa-discord text-2xl hover:text-blue-500 cursor-pointer"></i> */}
           <FontAwesomeIcon
-            className="text-3xl ph:w-[28px] ph:h-[28px] hover:text-blue-700 cursor-pointer transition duration-300"
+            className="w-[28px] h-[28px] hover:text-blue-700 cursor-pointer transition duration-300"
             icon={faDiscord}
             onClick={() =>
                 window.open("https://discord.com/invite/3VbUQME", "_self")
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,124 +1,127 @@
 export const Header = () => {
-  return (
-    <>
-      {/* main div  */}
-      <div className="bg-[#262522]  w-36 h-screen  flex flex-col text-gray-300 font-bold text-base">
-        {/* every thing div */}
-        <div className="bg-transparent w-full cursor-pointer hover:bg-[#21201E] py-2.5 flex justify-center">
-          <img
-            src="/icon.png"
-            alt="chess icon png"
-            className="bg-transparent  h-auto w-[7vw]"
-          />
-        </div>
+    return (
+        <>
+            {/* main div  */}
+            <div className="bg-[#262522]  w-36 h-screen  flex flex-col text-gray-300 font-bold text-base">
+                {/* every thing div */}
+                <div className="bg-transparent w-full ph:hidden cursor-pointer hover:bg-[#21201E] py-2.5 flex justify-center">
+                    <img
+                        src="/icon.png"
+                        alt="chess icon png"
+                        className="bg-transparent  h-auto w-[7vw]"
+                        onClick={() => {
 
-        {/*  hand with chess  */}
-        <div className="bg-transparent gap-3 flex pl-4 py-2.5 justify-start cursor-pointer hover:bg-[#21201E]  items-center">
-          <img
-            src="/hand-with-chess.svg"
-            alt="hand with chess svg image "
-            className="bg-transparent w-7 h-auto "
-          />
-          <p className="bg-transparent">Play</p>
-        </div>
+                        }}
+                    />
+                </div>
 
-        {/* puzlee */}
-        <div className="cursor-pointer hover:bg-[#21201E] bg-transparent flex  gap-3 pl-4 py-2.5 justify-start items-center">
-          <img
-            src="/puzlee.png"
-            alt="hand with chess svg image "
-            className="bg-transparent w-7 scale-150 h-auto "
-          />
-          <p className="bg-transparent">Puzzles</p>
-        </div>
+                {/*  hand with chess  */}
+                <div className="bg-transparent gap-3 flex pl-4 py-2.5 justify-start cursor-pointer hover:bg-[#21201E]  items-center">
+                    <img
+                        src="/hand-with-chess.svg"
+                        alt="hand with chess svg image "
+                        className="bg-transparent w-7 h-auto "
+                    />
+                    <p className="bg-transparent">Play</p>
+                </div>
 
-        {/* learn */}
-        <div className="cursor-pointer hover:bg-[#21201E] bg-transparent flex  gap-3 pl-4 py-2.5 justify-start items-center">
-          <img
-            src="/lessons.svg"
-            alt="hand with chess svg image "
-            className="bg-transparent w-7 h-auto "
-          />
-          <p className="bg-transparent">Learn</p>
-        </div>
+                {/* puzlee */}
+                <div className="cursor-pointer hover:bg-[#21201E] bg-transparent flex  gap-3 pl-4 py-2.5 justify-start items-center">
+                    <img
+                        src="/puzlee.png"
+                        alt="hand with chess svg image "
+                        className="bg-transparent w-7 scale-150 h-auto "
+                    />
+                    <p className="bg-transparent">Puzzles</p>
+                </div>
 
-        {/* Watch */}
-        <div className="cursor-pointer hover:bg-[#21201E] bg-transparent flex  gap-3 pl-4 py-2.5 justify-start items-center">
-          <img
-            src="/watch.svg"
-            alt="hand with chess svg image "
-            className="bg-transparent w-7 h-auto "
-          />
-          <p className="bg-transparent">Watch</p>
-        </div>
+                {/* learn */}
+                <div className="cursor-pointer hover:bg-[#21201E] bg-transparent flex  gap-3 pl-4 py-2.5 justify-start items-center">
+                    <img
+                        src="/lessons.svg"
+                        alt="hand with chess svg image "
+                        className="bg-transparent w-7 h-auto "
+                    />
+                    <p className="bg-transparent">Learn</p>
+                </div>
 
-        {/* News */}
-        <div className="cursor-pointer hover:bg-[#21201E] bg-transparent flex  gap-3 pl-4 py-2.5 justify-start items-center">
-          <img
-            src="/newspaper.svg"
-            alt="hand with chess svg image "
-            className="bg-transparent w-7 h-auto "
-          />
-          <p className="bg-transparent">News</p>
-        </div>
+                {/* Watch */}
+                <div className="cursor-pointer hover:bg-[#21201E] bg-transparent flex  gap-3 pl-4 py-2.5 justify-start items-center">
+                    <img
+                        src="/watch.svg"
+                        alt="hand with chess svg image "
+                        className="bg-transparent w-7 h-auto "
+                    />
+                    <p className="bg-transparent">Watch</p>
+                </div>
 
-        {/* social */}
-        <div className="cursor-pointer hover:bg-[#21201E] bg-transparent flex  gap-3 pl-4 py-2.5 justify-start items-center">
-          <img
-            src="/friend.svg"
-            alt="hand with chess svg image "
-            className="bg-transparent w-7 h-auto "
-          />
-          <p className="bg-transparent">Social</p>
-        </div>
+                {/* News */}
+                <div className="cursor-pointer hover:bg-[#21201E] bg-transparent flex  gap-3 pl-4 py-2.5 justify-start items-center">
+                    <img
+                        src="/newspaper.svg"
+                        alt="hand with chess svg image "
+                        className="bg-transparent w-7 h-auto "
+                    />
+                    <p className="bg-transparent">News</p>
+                </div>
 
-        {/* more */}
-        <div className="cursor-pointer hover:bg-[#21201E] bg-transparent flex  gap-3 pl-4 py-2.5 justify-start items-center">
-          <img
-            src="/more.svg"
-            alt="hand with chess svg image "
-            className="bg-transparent w-7 h-auto "
-          />
-          <p className="bg-transparent">More</p>
-        </div>
+                {/* social */}
+                <div className="cursor-pointer hover:bg-[#21201E] bg-transparent flex  gap-3 pl-4 py-2.5 justify-start items-center">
+                    <img
+                        src="/friend.svg"
+                        alt="hand with chess svg image "
+                        className="bg-transparent w-7 h-auto "
+                    />
+                    <p className="bg-transparent">Social</p>
+                </div>
 
-        {/* SEARCH BAR */}
-        <div className="bg-transparent text-base font-normal w-full px-2">
-          <input
-            type="text"
-            className="w-full border-2 border-gray-500 px-3 py-1 bg-[#373633] text-white rounded-md focus:outline-none"
-            placeholder="Search"
-          />
-        </div>
+                {/* more */}
+                <div className="cursor-pointer hover:bg-[#21201E] bg-transparent flex  gap-3 pl-4 py-2.5 justify-start items-center">
+                    <img
+                        src="/more.svg"
+                        alt="hand with chess svg image "
+                        className="bg-transparent w-7 h-auto "
+                    />
+                    <p className="bg-transparent">More</p>
+                </div>
 
-        {/* sign up */}
-        <div className=" w-full px-3 pt-6 bg-transparent">
-          <p className="bg-[#7ca43f] justify-center text-gray-300 cursor-pointer duration-200  hover:bg-[#8abc40] items-center flex py-2 rounded-md">
-            Sign up
-          </p>
-        </div>
+                {/* SEARCH BAR */}
+                <div className="bg-transparent text-base font-normal w-full px-2">
+                    <input
+                        type="text"
+                        className="w-full border-2 border-gray-500 px-3 py-1 bg-[#373633] text-white rounded-md focus:outline-none"
+                        placeholder="Search"
+                    />
+                </div>
 
-        {/* log out */}
-        <div className=" w-full px-3 pt-2 bg-transparent">
-          <p className=" justify-center cursor-pointer text-gray-400 duration-200  hover:bg-[#454441] items-center flex py-2 rounded-md">
-            Log out
-          </p>
-        </div>
+                {/* sign up */}
+                <div className=" w-full px-3 pt-6 bg-transparent">
+                    <p className="bg-[#7ca43f] justify-center text-gray-300 cursor-pointer duration-200  hover:bg-[#8abc40] items-center flex py-2 rounded-md">
+                        Sign up
+                    </p>
+                </div>
 
-        {/* footer part */}
-        <div className="mt-auto bg-[#262522] text-gray-500 text-xs w-full mb-4 flex flex-col ">
-          <div className="flex hover:bg-[#21201E] hover:text-white pl-4 pb-3 cursor-pointer w-full bg-transparent items-center gap-2">
-            <img src="/support.png" className="bg-transparent w-6 h-auto" alt="language option" />
-            <span className=" bg-transparent ">English</span>
-          </div>
-          <div className="flex hover:bg-[#21201E] hover:text-white pl-4 pb-3 cursor-pointer bg-transparent items-center gap-2">
-            <img src="/questionMark.png" className="bg-transparent  w-6 h-auto" alt="language option" />
-            <span className=" bg-transparent ">Support</span>
-          </div>
-        </div>
+                {/* log out */}
+                <div className=" w-full px-3 pt-2 bg-transparent">
+                    <p className=" justify-center cursor-pointer text-gray-400 duration-200  hover:bg-[#454441] items-center flex py-2 rounded-md">
+                        Log out
+                    </p>
+                </div>
+
+                {/* footer part */}
+                <div className="mt-auto bg-[#262522] text-[#989795] text-xs w-full mb-4 flex flex-col ">
+                    <div className="flex hover:bg-[#21201E] hover:text-white pl-4 pb-3 cursor-pointer w-full bg-transparent items-center gap-2">
+                        <img src="/support.png" className="bg-transparent w-6 h-auto" alt="language option" />
+                        <span className=" bg-transparent ">English</span>
+                    </div>
+                    <div className="flex hover:bg-[#21201E] hover:text-white pl-4 pb-3 cursor-pointer bg-transparent items-center gap-2">
+                        <img src="/questionMark.png" className="bg-transparent  w-6 h-auto" alt="language option" />
+                        <span className=" bg-transparent ">Support</span>
+                    </div>
+                </div>
 
 
-      </div>
-    </>
-  );
+            </div>
+        </>
+    );
 };
