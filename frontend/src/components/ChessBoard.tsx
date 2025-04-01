b468268813a5c06b9f79ef744b898e10a3454aa1
Surya-majhi32038 <suryamajhi32038@gmail.com>
2025-04-01 16:04:46 +0530

    Refactor Game component to simplify structure and remove unused code; update dependencies in package.json

--- frontend/src/components/ChessBoard.tsx
+++ frontend/src/components/ChessBoard.tsx
@@ -1,132 +1,237 @@
-import { Chess, Color, PieceSymbol, Square } from "chess.js";
-import { useState } from "react";
-import { MOVE } from "../screen/Game";
-// import React from 'react'
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+import { Chess, Square } from "chess.js";
 
-export const ChessBoard = ({
-  chess,
-  setBoard,
-  board,
-  socket,
-}: {
-  chess: Chess;
-  setBoard: any;
-  board: ({
-    square: Square;
-    type: PieceSymbol;
-    color: Color;
-  } | null)[][];
-  socket: WebSocket;
-}) => {
-  // console.log(board,socket)
-  const [from, setFrom] = useState<Square | null>(null);
-  // const [to, setTo] = useState<Square | null>(null);
+const socket = io("http://localhost:3000/");
+
+const chess = new Chess();
+
+const ChessBoard = () => {
+  const [board, setBoard] = useState(chess.board());
+  const [playerRole, setPlayerRole] = useState<string | null>(null);
+  const [sourceSquare, setSourceSquare] = useState<{
+    row: number;
+    col: number;
+  } | null>(null);
+
+  useEffect(() => {
+    const handlePlayersRole = (role: string) => {
+      setPlayerRole(role);
+      setBoard([...chess.board()]);
+    };
+
+    socket.on("playersRole", handlePlayersRole);
+    socket.on("spectatorRole", () => {
+      setPlayerRole(null);
+      setBoard([...chess.board()]);
+    });
+    socket.on("boardState", (fen: string) => {
+      chess.load(fen);
+      setBoard([...chess.board()]);
+    });
+    socket.on("move", (move: { from: Square; to: Square }) => {
+      chess.move(move);
+      setBoard([...chess.board()]);
+    });
+
+    return () => {
+      socket.off("playersRole", handlePlayersRole);
+      socket.off("spectatorRole");
+      socket.off("boardState");
+      socket.off("move");
+    };
+  }, []);
+
+  const handleMove = (
+    source: { row: number; col: number },
+    target: { row: number; col: number }
+  ) => {
+    const move = {
+      from: `${String.fromCharCode(97 + source.col)}${
+        8 - source.row
+      }` as Square,
+       to: `${String.fromCharCode(97 + target.col)}${8 - target.row}` as Square
+    //   promotion: "q",
+    };
+
+    if (chess.move(move)) {
+      socket.emit("move", move);
+      setBoard([...chess.board()]);
+    }
+  };
+  
   return (
-    <div className="text-black  rounded-md">
-      {board.map((row, i) => {
-        // console.log(board)
-        //    console.log(i)
-        return (
-          <div key={i} className="flex">
-            {row.map((square, j) => {
-           //   console.log("i=", i, " j=", j);
-              return (
-                <div
-                  onClick={() => {
-                    const squareRepresentation = (String.fromCharCode(97 + j) +
-                      "" +
-                      (8 - i)) as Square;
-                    // console.log(squareRepresentation)
-                    if (from == null) {
-                      setFrom(squareRepresentation);
-                    } else {
-                      socket.send(
-                        JSON.stringify({
-                          type: MOVE,
-                          payload: {
-                            from: from,
-                            to: squareRepresentation,
-                          },
-                        })
-                      );
-
-                      chess.move({
-                        from: from,
-                        to: squareRepresentation,
-                      });
-
-                      setBoard(chess.board());
-                      //  console.log(from, squareRepresentation);
-                      setFrom(null);
-                      // setTo(null);
-                    }
-                  }}
-                  key={j}
-                  className={`w-8  md:w-16 md:h-16 h-8 flex justify-center  cursor-pointer items-center ${
-                    (i + j) % 2 == 0 ? "bg-[#769656]" : "bg-[#EEEED2]"
-                  } relative`}
-                >
-                  {/* this part is for number and alphabat */}
-                  {j == 0 ? (
-                    <>
-                    <p
-                      className={`absolute ${
-                        (i + j) % 2 == 0 ? "text-[#EEEED2]" : "text-[#769656]"
-                      } top-0 left-1 bg-transparent font-bold`}
+    <>
+    <div
+      className={`text-black rounded-md ${
+          playerRole === "w" ? "" : "rotate-180"
+        } p-2`}
+        >
+      { board.map((row, rowIndex) => {
+          //  console.log(rowIndex);
+          return (
+              <div key={rowIndex} className="flex">
+                {row.map((square, squareIndex) => {
+                    return (
+                     
+                    <div
+                      key={`${rowIndex}-${squareIndex}`}
+                      className={`
+                            w-8  md:w-16 md:h-16 h-8 flex justify-center  cursor-pointer items-center ${
+                              (rowIndex + squareIndex) % 2 == 0
+                                ? "bg-[#769656]"
+                                : "bg-[#EEEED2]"
+                            } relative`}
+                      onDragOver={(e: React.DragEvent<HTMLDivElement>) =>
+                        e.preventDefault()
+                      }
+                      onDrop={(e: React.DragEvent<HTMLDivElement>) => {
+                        e.preventDefault();
+                        if (sourceSquare) {
+                          handleMove(sourceSquare, {
+                            row: rowIndex,
+                            col: squareIndex,
+                          });
+                          setSourceSquare(null);
+                        }
+                      }}
                     >
-                      {i + 1}
-                    </p>
-                    {i == 7 && (
-                      <p
-                        className={`absolute ${
-                          (i + j) % 2 == 0 ? "text-[#EEEED2]" : "text-[#769656]"
-                        } bottom-0 right-0 bg-transparent font-bold`}
-                      >
-                        a
-                      </p>
-                    )}
-                  </>
-                    // how to add here a character 'a' when i == 7 and 'a' is bottom-0 and right-0 
-                  ) : 
-                   
-                  i == 7 ? (
-                    <p
-                      className={`absolute ${
-                        (i + j) % 2 == 0 ? "text-[#EEEED2]" : "text-[#769656]"
-                      } ${
-                        j == 0
-                          ? "top-0 left-1"
-                          : `${i == 7 ? "bottom-0 right-0" : null}`
-                      }  bg-transparent font-bold`}
-                    >
-                      {String.fromCharCode(97 + j)}
-                    </p>
-                  ) : null}
+                      {/* this part is for number and alphabat */}
+                      {playerRole === "b" ? (
+                        squareIndex == 7 ? (
+                          <>
+                            <p
+                              className={`absolute ${
+                                (rowIndex + squareIndex) % 2 == 0
+                                  ? "text-[#EEEED2]"
+                                  : "text-[#769656]"
+                              } bottom-0 right-1 bg-transparent font-bold rotate-180`}
+                            >
+                              {8 - rowIndex}
+                            </p>
+                            {rowIndex == 0 && (
+                              <p
+                                className={`absolute ${
+                                  (rowIndex + squareIndex) % 2 == 0
+                                    ? "text-[#EEEED2]"
+                                    : "text-[#769656]"
+                                } top-0 left-1 bg-transparent font-bold rotate-180`}
+                              >
+                                a
+                              </p>
+                            )}
+                          </>
+                        ) : // how to add here a character 'a' when i == 7 and 'a' is bottom-0 and right-1
+                        rowIndex == 0 ? (
+                          <p
+                            className={`absolute ${
+                              (rowIndex + squareIndex) % 2 == 0
+                                ? "text-[#EEEED2]"
+                                : "text-[#769656]"
+                            } ${
+                              squareIndex == 0
+                                ? `${
+                                    rowIndex == 0
+                                      ? "top-0 left-1"
+                                      : "bottom-0 right-1"
+                                  }`
+                                : `${rowIndex == 0 ? "top-0 left-1" : null}`
+                            }  bg-transparent font-bold rotate-180`}
+                          >
+                            {String.fromCharCode(104 - squareIndex)}
+                          </p>
+                        ) : null
+                      ) : squareIndex == 0 ? (
+                        <>
+                          <p
+                            className={`absolute ${
+                              (rowIndex + squareIndex) % 2 == 0
+                                ? "text-[#EEEED2]"
+                                : "text-[#769656]"
+                            } top-0 left-1 bg-transparent font-bold `}
+                          >
+                            {rowIndex + 1}
+                          </p>
+                          {rowIndex == 7 && (
+                            <p
+                              className={`absolute ${
+                                (rowIndex + squareIndex) % 2 == 0
+                                  ? "text-[#EEEED2]"
+                                  : "text-[#769656]"
+                              } bottom-0 right-1 bg-transparent font-bold `}
+                            >
+                              a
+                            </p>
+                          )}
+                        </>
+                      ) : // how to add here a character 'a' when i == 7 and 'a' is bottom-0 and right-1
+                      rowIndex == 7 ? (
+                        <p
+                          className={`absolute ${
+                            (rowIndex + squareIndex) % 2 == 0
+                              ? "text-[#EEEED2]"
+                              : "text-[#769656]"
+                          } ${
+                            squareIndex == 0
+                              ? "top-0 left-1"
+                              : `${rowIndex == 7 ? "bottom-0 right-1" : null}`
+                          }  bg-transparent font-bold `}
+                        >
+                          {String.fromCharCode(97 + squareIndex)}
+                        </p>
+                      ) : null}
 
-                  {/* this part is for chess pik */}
-                  {square ? (
-                    <img
-                      className={`w-full ${
-                        square?.type === "p"
-                          ? `py-3 px-4`
-                          : `${
-                              square?.type === "k" ? `py-2 px-1 ` : `py-2 px-3`
-                            }`
-                      } h-full bg-transparent `}
-                      src={`/${
-                        square?.color === "b"
-                          ? square?.type
-                          : `${square?.type?.toUpperCase()} copy`
-                      }.png`}
-                      alt=""
-                    />
-                  ) : null}
-                </div>
-              );
-            })}
-          </div>
-        );
-      })}
+                      {square && (
+                        <img
+                          src={`/${
+                            square?.color === "b"
+                              ? square?.type
+                              : `${square?.type?.toUpperCase()} copy`
+                          }.png`}
+                          alt={square.type}
+                          className={`piece ${
+                            square.color === "w" ? "white" : "black"
+                          } w-full ${
+                            square?.type === "p"
+                              ? `py-3 px-4`
+                              : `${
+                                  square?.type === "k"
+                                    ? `py-2 px-1 `
+                                    : `py-2 px-3`
+                                }`
+                          } h-full bg-transparent ${
+                            playerRole === "w" ? "" : "rotate-180"
+                          }`}
+
+                          draggable={playerRole === square.color}
+                          onDragStart={(
+                            e: React.DragEvent<HTMLImageElement>
+                          ) => {
+                            if (playerRole === square.color) {
+                              setSourceSquare({
+                                row: rowIndex,
+                                col: squareIndex,
+                              });
+                              e.dataTransfer.setData("text/plain", "");
+                            }
+                          }}
+
+                    
+                        />
+                      )}
+
+                    </div>
+                  );
+                })}
+                ;
+              </div>
+            );
+          }) }
+
     </div>
+     <p className="text-white text-2xl">Your are : {playerRole == "w"? "White":"Black"}</p>
+    </>
   );
 };
+
+export default ChessBoard;
--- frontend/src/screen/Game.tsx
+++ frontend/src/screen/Game.tsx
@@ -1,78 +1,21 @@
-import { useEffect, useState } from "react";
-import { ChessBoard } from "../components/ChessBoard";
-import { useSockts } from "../hooks/useSockts";
-import Button from "./Button";
-import { Chess } from "chess.js";
+import { useState, useEffect } from "react";
+import ChessBoard from "../components/ChessBoard";
 
-export const INIT_GAME = "init_game";
-export const MOVE = "move";
-export const GAME_OVER = "game_over";
 const Game = () => {
-        const socket = useSockts();
-        const [chess, setChess] = useState(new Chess());
-        const [board, setBoard] = useState(chess.board());
-    const [started, setStarted] = useState(false)
-    const [waiting, setWaiting] = useState(false)
-    // console.log('below we have print the -> "new Chess()" ')
-    // console.log( Chess)
+ 
 
-    useEffect(() => {
-        if (!socket) {
-            return;
-        }
-        console.log()
-        socket.onmessage = (e) => {
-          //  console.log("chess -> ",chess,"board -> ",board,"sockets ->",e.data) 
-            const message = JSON.parse(e.data);
-            // console.log(message);
-            switch (message.type) {
-                case INIT_GAME:
-                //    setChess(new Chess());
-                    setBoard(chess.board());
-                    setStarted(true);
-                    console.log("Game initialized");
-                    break;
-                case MOVE:
-                    const move = message.payload;
-                    chess.move(move);
-                    setBoard(chess.board());
-                    console.log("Move made");
-                    break;
-                case GAME_OVER:
-                    console.log("Game over");
-                    break;
-            }
-        };
-    }, [socket]);
+//   useEffect(() => {
+//     // If sessionStorage is empty, reset the board
+//     if (!sessionStorage.getItem("chessboard")) {
+//       setKey(prevKey => prevKey + 1); // Change key to force re-render
+//     }
+//   }, []);
 
-    if (!socket) return <div>Connecting...</div>;
-    return (
-        <div className="flex justify-center">
-            <div className="pt-8 max-w-screen-lg w-full">
-                <div className="grid grid-cols-6 gap-4 w-full ">
-                    <div className="col-span-4 bg-red-200 w-full">
-                        <ChessBoard chess={chess} setBoard={setBoard} board={board} socket={socket} />
-                        
-                    </div>
-                    <div className="col-span-2 flex items-center justify-center bg-green-200 w-full">
-                         <Button
-                            onClick={() => {
-                                setWaiting(!waiting)
-                               // console.log("started state :",started," waiting state : ",waiting)
-                                socket.send( // this send message recived in backend part -> GameManager.ts -> addHandler -> INIT_GAME 
-                                    JSON.stringify({
-                                        type: INIT_GAME, // The function JSON.stringify() converts a JavaScript object or value into a JSON-formatted string.
-                                    })
-                                );
-                            }}
-                        >
-                            Play    
-                        </Button>
-                        
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex justify-center items-center h-screen">
+    <ChessBoard  /> 
+    </div>
+  );
 };
+
 export default Game;
