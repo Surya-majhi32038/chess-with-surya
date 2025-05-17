c77f8cdeddbf0cb39165eebfd89dbb276ca4b02a
Surya-majhi32038 <suryamajhi32038@gmail.com>
2025-05-17 20:02:45 +0530

    update the code

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,11 @@
-require("dotenv").config({path:'./.env'});
+
 
 const express = require("express");
 import { createServer } from "http";
 import { Server, Socket } from "socket.io";
 import { Chess } from "chess.js";
 import path from "path";
-import connectDB from "./db/index";
-console.log("i'm in backend ");
+// console.log("i'm in backend ");
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
@@ -26,9 +25,6 @@ app.get('/', (req: any, res: any) => {
 //     white?: string;
 //     black?: string;
 // }
-
-// Data base connectio function called 
-connectDB();
 
 class PlayersClass {
     public white: string;
@@ -51,14 +47,14 @@ let pendingUser: string | null;
 pendingUser = null
 let currentPlayers: "w" | "b" = "w";
 io.on("connection", (socket: Socket) => {
-    console.log("New client connected", socket.id);
+    // console.log("New client connected", socket.id);
     // console.log("Games array ",Games)
     // try to find any plaers have any type like white or black empty or not
     let player: any;
     player = Games.find(game =>
         (game.white && !game.black) || (!game.white && game.black)
     );
-    
+
     if (player) { // if any players are in the game(white or black)
         console.log("i'm in player")
         if (player.white) {
@@ -98,8 +94,14 @@ io.on("connection", (socket: Socket) => {
     socket.on("disconnect", () => {
         // console.log("Client disconnected", socket.id);
         // console.log("Games array before disconnect",Games)
-        Games = Games.filter(game => game.white  || game.black);
+        Games = Games.filter(game => game.white || game.black); // if any of the player is not present then remove the game from the array
         // console.log("Games array disconnect",Games)
+        Games = Games.filter(game =>
+            !(
+                (game.white === socket.id && game.black == null) ||
+                (game.black === socket.id && game.white == null)
+            )
+        );
 
         let players: any;
         players = Games.find(p => p.black === socket.id || p.white === socket.id);
@@ -113,12 +115,12 @@ io.on("connection", (socket: Socket) => {
 
         if (socket.id === players?.white) {
             players.white = ""
-            io.to(players.black).emit("boardState", players.chess.fen(),players.chess.history({ verbose: true }));
+            io.to(players.black).emit("boardState", players.chess.fen(), players.chess.history({ verbose: true }));
             io.to(players.black).emit("opponetGone");
 
         } else {
             players.black = ""
-             io.to(players.white).emit("boardState", players.chess.fen(),players.chess.history({ verbose: true }));
+            io.to(players.white).emit("boardState", players.chess.fen(), players.chess.history({ verbose: true }));
             io.to(players.white).emit("opponetGone");
         }
 
@@ -135,10 +137,11 @@ io.on("connection", (socket: Socket) => {
     );
 
     socket.on("move", (move: any) => {
+        console.log("after every movement ", Games)
         let players: any;
         players = Games.find(p => p.black === socket.id || p.white === socket.id);
         // console.log("move received:", move);
-        
+
         // console.log(players?.chess?.history({ verbose: true }))
         try {
             if ((players.chess.turn() === "w" && socket.id !== players?.white) ||
@@ -150,10 +153,10 @@ io.on("connection", (socket: Socket) => {
             if (result) {
                 currentPlayers = players.chess.turn();
                 io.to(players.white).emit("move", move)
-                 io.to(players.white).emit("boardState", players.chess.fen(),players.chess.history({ verbose: true }));
+                io.to(players.white).emit("boardState", players.chess.fen(), players.chess.history({ verbose: true }));
 
                 io.to(players.black).emit("move", move)
-                 io.to(players.black).emit("boardState", players.chess.fen(),players.chess.history({ verbose: true }));
+                io.to(players.black).emit("boardState", players.chess.fen(), players.chess.history({ verbose: true }));
 
                 // io.emit("move", move); // Broadcast move event
                 // io.emit("boardState", chess.fen());
@@ -163,8 +166,8 @@ io.on("connection", (socket: Socket) => {
 
                     io.to(players.white).emit("Checkmate")
                     io.to(players.black).emit("Checkmate")
-                     io.to(players.black).emit("boardState", players.chess.fen(),players.chess.history({ verbose: true }));
-                     io.to(players.black).emit("boardState", players.chess.fen(),players.chess.history({ verbose: true }));
+                    io.to(players.black).emit("boardState", players.chess.fen(), players.chess.history({ verbose: true }));
+                    io.to(players.black).emit("boardState", players.chess.fen(), players.chess.history({ verbose: true }));
                     // io.emit("Checkmate")
                     // io.emit("boardState", chess.fen());
                     players.chess.reset();
@@ -198,13 +201,13 @@ io.on("connection", (socket: Socket) => {
         }
         if (players) {
             players.chess.undo();
-             io.to(players.white).emit("boardState", players.chess.fen(),players.chess.history({ verbose: true }));
-             io.to(players.black).emit("boardState", players.chess.fen(),players.chess.history({ verbose: true }));
+            io.to(players.white).emit("boardState", players.chess.fen(), players.chess.history({ verbose: true }));
+            io.to(players.black).emit("boardState", players.chess.fen(), players.chess.history({ verbose: true }));
         }
     });
 
     // after game complitation (like - any user gone, win, other case )
-    socket.on("reConnect",()=>{
+    socket.on("reConnect", () => {
         //1. destroy the corrent game 
 
         //2. find a new game , if present then start the game 
@@ -213,6 +216,6 @@ io.on("connection", (socket: Socket) => {
 
 });
 
-server.listen(9000, () => { 
+server.listen(9000, () => {
     console.log("Listening on port 3000");
 });
